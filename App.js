e49e3a13b76918b1bf0bdaff6740a58508c5658b
onepice0807 <onepice0807@gmail.com>
2023-10-30 19:17:47 +0900

    2023-10-30 리엑트 네이티브
    1) 안드로이드폰에 Expo go 설치
    2) - Expo 프로젝트 생성하기     --1)Expo CLI 설치
        npm install -g expo-cli
         npx create-expo-app
    3) App.js에 Image, TextInput, ScrollView태그등을 사용해보기

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import pikachu2 from "./assets/pikachu2.png";
+import { useState } from "react";
 
 export default function App() {
+  const [isTrue, setIsTrue] = useState(false);
+  const [text, setText] = useState("");
+
+  console.log(text);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Text>Hello React Native</Text>
+        <StatusBar style="auto" />
+        <Text style={{ fontSize: 20, color: "pink", fontWeight: 20 }}>
+          드디어 네이티브다!!!
+        </Text>
+        <Image
+          source={{
+            uri: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/001801.png",
+          }}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image source={pikachu2} style={styles.pikachu2} />
+        <Image
+          source={{
+            uri: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/002601.png",
+          }}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={{
+            uri: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/000301.png",
+          }}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={{
+            uri: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/000901.png",
+          }}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={{
+            uri: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/000601.png",
+          }}
+          style={{ width: 400, height: 400 }}
+        />
+        <TextInput
+          placeholder="이름을 입력하세요!!!"
+          onChangeText={setText}
+          value={text}
+        />
+        <Switch value={isTrue} onValueChange={() => setIsTrue(!isTrue)} />
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +70,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 0,
+  },
+
+  pikachu2: {
+    width: 400,
+    height: 400,
+    padding: 0,
+    marginTop: 10,
   },
 });
